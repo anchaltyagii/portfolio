2c9340a838991267a04704e63b023c8573c13d11
anchaltyagii <anchaltyagi000@gmail.com>
2023-01-01 19:45:59 +0530

    modification in css

--- src/components/About.js
+++ src/components/About.js
@@ -9,31 +9,25 @@ function About() {
         <h1 className="about-header">Who Am I</h1>
         <p>
           I am a Web developer, Currently living in Noida. <br></br>I love to
-          buld websites and talk about creativity
+          build websites and talk about creativity.
+        </p>
+        <p>
+          I am working as Software Developer (Front-end) with <br /> experience
+          of <strong>1+ year</strong> in IT industry.
         </p>
         <h3>Education:</h3>
         <p>
           I have completed Bachelor Of Technology(B.Tech) <br></br> from
           Banasthali University, Jaipur in 2021.
         </p>
-      </div>
-      <div className="skills-container">
         <p className="tech-stack">Tech Stack:</p>
-        <ul className="skills-li1">
-          <li>JavaScript</li>
-          <li>ReactJs</li>
-          <li>Redux</li>
-          <li>HTML5</li>
-          <li>CSS</li>
-        </ul>
-        <ul className="skills-li2">
-          <li>Java</li>
-          <li>PHP</li>
-          <li>C</li>
-          <li>C++</li>
-        </ul>
+        <p>
+          I am Proficient in tech stack like, JavaScript, ReactJs,<br /> 
+           Redux, Nextjs, HTML5, CSS, SASS.
+        </p>
+        <p></p>
       </div>
-      <div className="tech_img_wrapper">
+      <div className="tech_img_wrapper ">
         <img src={logo} className="App-logo" alt="technology" />
       </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,18 @@ function Home() {
   return (
     <>
       <div className="Home-wrapper">
-        <h1 className="home-info">Hi,</h1>
-        <h1 className="home-info">I'm Anchal Tyagi</h1>
-        <h1 className="home-info">Web developer</h1>
-        <h6 className="home-info2">Software Engineer (Front-end)</h6>
-        <NavLink className="nav-content1" exact to="/contact">
-          <div className="contact-button">Contact Me!</div>
-        </NavLink>
-      </div>
-      <div className="img_wrapper">
-        <img src={Home_web} className="img" alt="Home-image" />
+        <div>
+          <h1 className="home-info">Hi,</h1>
+          <h1 className="home-info">I'm Anchal Tyagi</h1>
+          <h1 className="home-info">Web developer</h1>
+          <h6 className="home-info2">Software Engineer (Front-end)</h6>
+          <NavLink className="nav-content1" exact to="/contact">
+            <div className="contact-button">Contact Me!</div>
+          </NavLink>
+        </div>
+        <div className="img_wrapper">
+          <img src={Home_web} className="img" alt="Home-image" />
+        </div>
       </div>
     </>
   );
